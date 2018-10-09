7cb55bea2b5096a791c4e46632181937cd9b0982
Bink Wang <fxmmc@sina.com>
2018-10-09 16:15:23 +1300

    Remove unused classes/functions

--- EmployeeSignIn/Extension/UIColor+.swift
+++ EmployeeSignIn/Extension/UIColor+.swift
@@ -11,13 +11,6 @@ import Foundation
 
 extension UIColor {
     
-    static var random: UIColor {
-        let red:CGFloat = CGFloat(drand48())
-        let green:CGFloat = CGFloat(drand48())
-        let blue:CGFloat = CGFloat(drand48())
-        return UIColor(red:red, green: green, blue: blue, alpha: 1.0)
-    }
-    
     static var light: UIColor {
         return UIColor(red:0.9, green: 0.9, blue: 0.9, alpha: 1.0)
     }
--- EmployeeSignIn/Extension/UIImage+.swift
+++ EmployeeSignIn/Extension/UIImage+.swift
@@ -11,17 +11,17 @@ import UIKit
 
 extension UIImage {
     
-    func imageWithColor(_ color: UIColor) -> UIImage? {
-        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()
-        
-        context?.setFillColor(color.cgColor)
-        context?.fill(rect)
-        
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return image
-    }
+//    func imageWithColor(_ color: UIColor) -> UIImage? {
+//        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+//        UIGraphicsBeginImageContext(rect.size)
+//        let context = UIGraphicsGetCurrentContext()
+//        
+//        context?.setFillColor(color.cgColor)
+//        context?.fill(rect)
+//        
+//        let image = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        
+//        return image
+//    }
 }
